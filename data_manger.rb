aba69c07c072b7672bf3586618a710d5ae6cecd5
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-11-15 06:37:36 +0300

    Fix linter errors

--- data_manger.rb
+++ data_manger.rb
@@ -1,40 +1,38 @@
 require 'json'
 
 class DataManger
-  Data_path = './data/'
-
   def self.load_books
-    if File.exist?(Data_path + 'books.json')
-      books = JSON.parse(File.read(Data_path + 'books.json')).map do |book|
+    if File.exist?('./data/books.json')
+      books = JSON.parse(File.read('./data/books.json')).map do |book|
         Book.new(book['title'], book['author'], id: book['id'])
       end
     else
-      File.write(Data_path + 'books.json', JSON.dump([]))
+      File.write('./data/books.json', JSON.dump([]))
     end
     books.any? ? books : []
   end
 
   def self.save_book(books)
-    if books
-      File.write(Data_path + 'books.json', JSON.dump(books))
+    if books.any?
+      File.write('./data/books.json', JSON.dump(books))
     else
-      File.write(Data_path + 'books.json', JSON.dump([]))
+      File.write('./data/books.json', JSON.dump([]))
     end
   end
 
   def self.save_people(people)
-    if people?
-      File.write(Data_path + 'people.json', JSON.dump(people))
+    if people
+      File.write('./data/people.json', JSON.dump(people))
     else
-      File.write(Data_path + 'people.json', JSON.dump([]))
+      File.write('./data/people.json', JSON.dump([]))
     end
   end
 
   def self.save_rentals(rentals)
-    if rentals?
-      File.write(Data_path + 'rentals.json', JSON.dump(rentals))
+    if rentals
+      File.write('./data/rentals.json', JSON.dump(rentals))
     else
-      File.write(Data_path + 'rentals.json', JSON.dump([]))
+      File.write('./data/rentals.json', JSON.dump([]))
     end
   end
 end
