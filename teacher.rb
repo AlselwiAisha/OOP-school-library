594026bbd320a75f8ded805d27215be81972df20
Al-Amin <alaminkhanshakil@gmail.com>
2023-11-17 04:05:34 +0600

    Add test for rentals

--- teacher.rb
+++ teacher.rb
@@ -2,6 +2,7 @@ require_relative 'person'
 
 class Teacher < Person
   attr_accessor :specialization
+
   def initialize(age, specialization, id: Random.rand(1..1000), name: 'Unknown', parent_permission: true)
     super(age, id: id, name: name, parent_permission: parent_permission)
     @specialization = specialization
